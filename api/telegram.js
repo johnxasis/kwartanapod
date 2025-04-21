8c1f3bd845fbfb131047cd414bee2127875db454
johnxasis <johnxasis@gmail.com>
2025-04-21 18:00:41 +0800

    Add files via upload

--- api/telegram.js
+++ api/telegram.js
@@ -1,11 +1,9 @@
-import express from "express";
-const app = express();
-app.use(express.json());
-
-app.post("/", (req, res) => {
-    const message = req.body?.message?.text || "No message";
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const message = req.body?.message?.text || 'No message';
     console.log(`Telegram Message: ${message}`);
-    res.status(200).send("OK");
-});
-
-export default app;
+    return res.status(200).send('OK');
+  } else {
+    return res.status(405).send('Method Not Allowed');
+  }
+}
