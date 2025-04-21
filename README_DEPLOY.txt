b6772ce1861ea79e9c44a639b5b6d0d8f57c1c55
johnxasis <johnxasis@gmail.com>
2025-04-21 15:39:22 +0800

    Add files via upload

---
+++ README_DEPLOY.txt
@@ -0,0 +1,3 @@
+1. Rename env.txt to .env
+2. Push to GitHub
+3. Auto-deploy on Vercel.
--- index.js
+++ index.js
@@ -1,9 +1,1 @@
 // Entry point
-require('dotenv').config();
-const { initTelegram } = require('./lib/telegram');
-const { runTasks } = require('./swarm/tasks');
-
-(async () => {
-  await runTasks();
-  initTelegram();
-})();
